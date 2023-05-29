77f29bce1df9e998413ca24aa13debae01793cb3
Asylniet <zasylniet@mail.ru>
2023-05-29 09:38:18 +0600

    small tweaks

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -10,8 +10,8 @@ const Header = () => {
           <div className="row align-items-center">
             <div className="col-lg-6">
               <ul className="top-bar-info list-inline-item pl-0 mb-0">
-                <li className="list-inline-item"><a href="mailto:support@gmail.com"><i className="icofont-support-faq mr-2"></i>support@med.com</a></li>
-                <li className="list-inline-item"><i className="icofont-location-pin mr-2"></i>Address Ta-134/A, Almaty, Kazakhstan </li>
+                <li className="list-inline-item"><a href="https://instagram.com/hijama_center?igshid=MzRlODBiNWFlZA=="><i className="icofont-support-faq mr-2"></i>instagram</a></li>
+                <li className="list-inline-item"><i className="icofont-location-pin mr-2"></i>Райымбек 212а/7, Almaty, Kazakhstan </li>
               </ul>
             </div>
             <div className="col-lg-6">
